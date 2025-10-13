630f407fc2137ed1a41375b4dba1205984142af0
GAURAV SINGH <thakurgaurav7820@gmail.com>
2025-10-13 19:14:13 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
-$username = "root";
+$username = "gaurav";
 $password = "";
 $dbname = "event_management";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);//predefined class
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
